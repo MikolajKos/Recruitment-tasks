03485109bd12364f663b8fea0b8f58c9c043d08a
MikolajKos <niko.kosiorek@gmail.com>
2022-11-02 22:23:18 +0100

    Palindrome Finished

---
+++ recruitment tasks/Palindrome/Palindrome/Palidrome.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    public class Palidrome
+    {
+        public bool IsStringPalindrome(string userString)
+        {
+            string trimedString = TrimStringInside(userString);
+            //List<string> myList = new List<string>();
+            string result = string.Empty;
+
+            for (int i = 0; i < trimedString.Length; i++)
+            {
+                
+                
+                result += trimedString[(trimedString.Length - 1) - i].ToString();
+                
+                //myList.Add(trimedString[trimedString.Length - i].ToString());
+            }
+
+            if (trimedString == result)
+                return true;
+            return false;
+        }
+
+        private string TrimStringInside(string userString)
+        {
+            List<string> myResult = new List<string>();
+            string result ="";
+
+            for (int i = 0; i < userString.Length; i++)
+            {
+                if (userString[i] != ' ') result += userString[i].ToString();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ recruitment tasks/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chceck if your string is palindrome:");
+            string myPalindrome = Console.ReadLine();
+
+            Palidrome palidrome = new Palidrome();
+
+            if (palidrome.IsStringPalindrome(myPalindrome))
+                Console.WriteLine("Palidrome!");
+            else Console.WriteLine("Not a Palindrome.");
+
+        }
+
+
+    }
+}
